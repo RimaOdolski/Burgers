011d17e13a0c3f794a6b6be5410b8147cd13e2a3
Rima O <rymaothmani@gmail.com>
2020-03-19 14:41:35 -0500

    create database and table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BaconCouscous', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double', false);
